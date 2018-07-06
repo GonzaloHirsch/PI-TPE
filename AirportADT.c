c8eacff312897efdfadf7ceedac02547c26623c5
Fletterio <40742817+Fletterio@users.noreply.github.com>
2018-07-06 11:55:36 -0300

    Funciones dependientes de MovementADT modificadas

--- AirportADT.c
+++ AirportADT.c
@@ -9,40 +9,38 @@ typedef struct AirportCDT {
     tIATA iata;
     Denomination denomination;
     TTraffic traffic;
-    ListADT movements;
+    MovementList movements;
     int unknownDepartures;
     int unknownArrivals;
  } AirportCDT;
 
-OACI getAirportOACI(AirportADT airport){
- 	return airport->oaci;
- }
-
  AirportADT newAirport(OACI oaci){
  	AirportADT retVal = malloc(sizeof(AirportCDT));
  	strcpy(retVal->oaci, oaci);
- 	retVal->movements = newList((OACI (*)(void *)) getMovementOACI);         //funcion de MovementADT.h
+ 	retVal->movements =  newMovementList();         //funcion de MovementADT.h
  	return retVal;
  }
 
  void setIATA(AirportADT airport, IATA iata){
  	strcpy(airport->iata, iata);
  }
-/*
- void setDenomination(AirportADT airport, Denomination denomination){
- 	Lo dejé vacío  porque para mí descripción debería ser char*. Sino se complican los prints
- }
 
-*/
+ void setDenomination(AirportADT airport, Denomination denomination){
+ 	strcpy(airport->denomination, iata);
+ }
 
  void setTraffic(AirportADT airport, TTraffic traffic){
  	airport->traffic = traffic;
  }
 
+ OACI getAirportOACI(AirportADT airport){
+ 	return airport->oaci;
+ }
+
  void addMovement(AirportADT airport, MovementADT movement){
- 	addElem(airport->movements, &movement);                         //funcion de listADT.h
+ 	addMovementElem(airport->movements, movement);                         //funcion de listADT.h
  }
 
 MovementADT getMovement(AirportADT airport, OACI oaci){
-	return getElem(airport->movements, oaci);                       //funcion de listADT.h
+	return getMovementElem(airport->movements, oaci);                       //funcion de listADT.h
 }
